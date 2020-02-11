03cd60c3f97a505f8444737480ebbfc260446983
Jennifer Payano <arlenepayano@gmail.com>
2020-02-11 14:30:46 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 
+let cards= ['queen', 'king','queen', 'king'];
 
-let cardOne="queen";
-let cardTwo= "king";
-let cardThree= "queen";
-let cardFour="king";
+let cardsInPlay=[];
 
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You have found a match!");
+}
+else {
+	alert("Sorry, Try Again");
+}
+
+}
