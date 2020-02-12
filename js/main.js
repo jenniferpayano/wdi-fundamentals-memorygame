a2e48bd40adc691c48a087dff42fdb68c778e62f
Jennifer Payano <arlenepayano@gmail.com>
2020-02-11 22:29:37 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 
-let cards= ['queen', 'king','queen', 'king'];
+const cards= [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+	suit: "hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 
 let cardsInPlay=[];
 
@@ -8,6 +31,7 @@ function checkForMatch() {
 if (cardsInPlay.length === 2) {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("You have found a match");
+
 //alert("You have found a match!");
 }
 else {
@@ -18,11 +42,16 @@ else {
 }
 
 function flipCards(cardOne , cardTwo){
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+cardsInPlay.push(cardOne.rank);
+cardsInPlay.push(cardTwo.rank);
+console.log("User flipped " + cardOne.rank);
+console.log(cardOne.cardImage);
+console.log(cardOne.suit);
+console.log("User flipped " + cardTwo.rank);
+console.log(cardTwo.cardImage);
+console.log(cardOne.suit);
 checkForMatch();
 }
 
-flipCards(cards[0], cards[1]);
+flipCards(cards[0], cards[2]);
